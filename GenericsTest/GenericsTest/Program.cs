22e52de6b4601b1d01c8cb33943f82d3f0671e3b
tutorial6 <manapure.vaibhav542@gmail.com>
2022-06-27 00:43:19 +0530

    Add UC1 Max Integer Value

---
+++ GenericsTest/GenericsTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GenericsTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 16;
+            int num2 = 22;
+            int num3 = 32;
+            GenericMaximum<int> maximum = new GenericMaximum<int>(num1, num2, num3);
+            int max = maximum.MaxMethod();
+            Console.WriteLine(max +" is Maximum Value");
+        }
+    }
+}
