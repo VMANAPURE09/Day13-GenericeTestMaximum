8579d62f221eaaa53c648dbe8a5f0fc43c8e3c06
tutorial6 <manapure.vaibhav542@gmail.com>
2022-06-27 12:21:06 +0530

    TC-1.3 Max Number 3rd Postion

--- GenericsTest/GenericsTest/Program.cs
+++ GenericsTest/GenericsTest/Program.cs
@@ -8,7 +8,7 @@ namespace GenericsTest
         {
             int num1 = 10;
             int num2 = 30;
-            int num3 = 20;
+            int num3 = 40;
             MaximumNumber max = new MaximumNumber();
             int maxNumber = max.ReturnMaxNumber(num1, num2, num3);
             Console.WriteLine("max number is " + maxNumber);
